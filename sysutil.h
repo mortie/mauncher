b723dc9d8f2ef2dd6232917d4d2b9cc9790f4ec6
Martin Dørum <martid0311@gmail.com>
2019-08-26 14:11:23 +0200

    generic binary search lookup

---
+++ sysutil.h
@@ -0,0 +1,18 @@
+#ifndef SYSUTIL_H
+#define SYSUTIL_H
+
+#include <stddef.h>
+
+char *string_concat(char **strs);
+char *read_until(int fd, char c, size_t *len);
+char *read_all(int fd, size_t *len);
+char **bs_lookup(const char *prefix, char **strs, size_t len, int (*cmp)(const char *a, const char *b, size_t n));
+
+char *xdg_runtime_dir();
+char *xdg_data_dirs();
+char *xdg_config_dirs();
+char *xdg_config_home();
+char *xdg_cache_home();
+char *xdg_data_home();
+
+#endif
